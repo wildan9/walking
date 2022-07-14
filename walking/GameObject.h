8e36e60ffcdf7156b9caa7fc4025a6d166732eff
route-x <optimus102001@gmail.com>
2022-07-14 23:55:27 +0700

    Add GameObject.cpp and move some code to GameObject.cpp

--- walking/GameObject.h
+++ walking/GameObject.h
@@ -2,10 +2,10 @@
 
 #pragma once
 
-#include "Player.h"
 #include "Map.h"
-#include "Animal.cpp"
+#include "Player.h"
 #include "Prop.cpp"
+#include "Animal.cpp"
 
 class GameObject
 {
@@ -26,56 +26,3 @@ private:
         else return 0;
     }
 };
-
-inline void GameObject::PlayWalkSound()
-{
-    if (player.GetPosition().x < -15.0f || player.GetPosition().y < 0.0f ||
-        player.GetPosition().x > ((float)map.GetDreamlandSize().width - 15.0f) * map.GetMapScale() &&
-        player.GetPosition().x < (float)map.GetDreamlandSize().width + 1500.0f ||
-        player.GetPosition().y > (float)map.GetDreamlandSize().height * map.GetMapScale()) player.OnWater();
-    else player.OnLand();
-}
-
-inline void GameObject::CheckCollision()
-{
-    if (CheckCollisionRecs(player.GetCollision(), animals.crocodile.GetCollision()) &&
-        player.IsPunch() && on_touch(&player, animals.crocodile.GetFacing(), animals.crocodile.GetPosition().x))
-    {
-        animals.crocodile.Hurt();
-    }
-    else
-    {
-        animals.crocodile.Walk();
-    }
-
-    for (auto& rhino : animals.rhinos) if (CheckCollisionRecs(player.GetCollision(), rhino.GetCollision())) player.Stop();
-
-    if (CheckCollisionRecs(player.GetCollision(), map.GetMapLine1()) ||
-        CheckCollisionRecs(player.GetCollision(), map.GetMapLine2())) player.Stop();
-
-    for (auto& invisibleFence : invisibleFences)
-    {
-        if (CheckCollisionRecs(player.GetCollision(),
-            Rectangle{ invisibleFence.x, invisibleFence.y, invisibleFence.width, invisibleFence.height })) player.Stop();
-    }
-
-    if (CheckCollisionRecs(player.GetCollision(), prop.naturalObj.GetBigStone1Coll()))
-    {
-        player.SetPosition({ 3400.0f - 80.0f, 600.0f });
-        player.SetTeleportStatus(1);
-    }
-
-    if (CheckCollisionRecs(player.GetCollision(), prop.naturalObj.GetBigStone2Coll()))
-    {
-        player.SetPosition({ 1600.0f - 80.0f, 700.0f });
-        player.SetTeleportStatus(1);
-    }
-}
-
-inline void GameObject::Draw(float deltaTime)
-{
-    map.Draw();
-    player.Draw();
-    prop.Draw();
-    animals.Draw(deltaTime);
-}
